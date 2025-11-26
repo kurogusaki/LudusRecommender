340ec5450fa74dcf79beec860ec0b3fe85e5938f
Yahia Amr <kormitpepe101@gmaiil.com>
2025-11-26 03:54:41 +0200

    v0.45 Game Genre,Platform,ReleaseYear Search Implementation

--- LudusRecommender.sql
+++ LudusRecommender.sql
@@ -114,72 +114,66 @@ AS
 GO
 
 -- Game Genre Search 
-/* Game Genre List - [Puzzle, Indie, Adventure, FPS, Rhythm, Action, Roguelike]
-*/
-CREATE PROCEDURE PuzzleGames
+CREATE PROCEDURE GameGenreSearch
+@Genre1 VARCHAR(50),
+@Genre2 VARCHAR(50),
+@Genre3 VARCHAR(50),
+@Genre4 VARCHAR(50)
+AS
+	BEGIN
+	IF @Genre2 IS NULL AND @Genre3 IS NULL AND @Genre4 IS NULL
+		BEGIN
+			SELECT GL.Game, GL.Genre, GL.Platform, GL.ReleaseYear
+			FROM GameLog GL
+			WHERE GL.Genre LIKE '%' + @Genre1 + '%'
+		END
+	ELSE IF @Genre3 IS NULL AND @Genre4 IS NULL
+		BEGIN
+			SELECT GL.Game, GL.Genre, GL.Platform, GL.ReleaseYear
+			FROM GameLog GL
+			WHERE GL.Genre LIKE '%' + @Genre1 + '%'
+			AND GL.Genre LIKE '%' + @Genre2 + '%'
+		END
+	ELSE IF @Genre4 IS NULL
+		BEGIN
+			SELECT GL.Game, GL.Genre, GL.Platform, GL.ReleaseYear
+			FROM GameLog GL
+			WHERE GL.Genre LIKE '%' + @Genre1 + '%'
+			AND GL.Genre LIKE '%' + @Genre2 + '%'
+			AND GL.Genre LIKE '%' + @Genre3 + '%'
+		END
+	ELSE
+	BEGIN
+			SELECT GL.Game, GL.Genre, GL.Platform, GL.ReleaseYear
+			FROM GameLog GL
+			WHERE GL.Genre LIKE '%' + @Genre1 + '%'
+			AND GL.Genre LIKE '%' + @Genre2 + '%'
+			AND GL.Genre LIKE '%' + @Genre3 + '%'
+			AND GL.Genre LIKE '%' + @Genre4 + '%'
+		END
+	END;
+GO
+-- Game Platform Search
+CREATE PROCEDURE GamePlatformSearch
+@Platform VARCHAR(50)
 AS
 	BEGIN
 		SELECT GL.Game, GL.Genre, GL.Platform, GL.ReleaseYear
 		FROM GameLog GL
-		WHERE GL.Genre LIKE '%Puzzle%'
+		WHERE GL.Platform LIKE '%' + @Platform + '%'
 	END;
 GO
-
-CREATE PROCEDURE IndieGames
+-- Game Release Year Search
+CREATE PROCEDURE GameReleaseYearSearch
+@ReleaseYear INT
 AS
 	BEGIN
 		SELECT GL.Game, GL.Genre, GL.Platform, GL.ReleaseYear
 		FROM GameLog GL
-		WHERE GL.Genre LIKE '%Indie%'
+		WHERE GL.ReleaseYear = @ReleaseYear
 	END;
 GO
-
-CREATE PROCEDURE AdventureGames
-AS
-	BEGIN
-		SELECT GL.Game, GL.Genre, GL.Platform, GL.ReleaseYear
-		FROM GameLog GL
-		WHERE GL.Genre LIKE '%Puzzle%'
-	END;
-GO
-
-CREATE PROCEDURE FPSGames 
-AS
-	BEGIN
-		SELECT GL.Game, GL.Genre, GL.Platform, GL.ReleaseYear
-		FROM GameLog GL
-		WHERE GL.Genre LIKE '%FPS%'
-	END;
-GO
-
-CREATE PROCEDURE RhythmGames 
-AS
-	BEGIN
-		SELECT GL.Game, GL.Genre, GL.Platform, GL.ReleaseYear
-		FROM GameLog GL
-		WHERE GL.Genre LIKE '%Rhythm%'
-	END;
-GO
-
-CREATE PROCEDURE ActionGames
-AS
-	BEGIN
-		SELECT GL.Game, GL.Genre, GL.Platform, GL.ReleaseYear
-		FROM GameLog GL
-		WHERE GL.Genre LIKE '%Action%'
-	END;
-GO
-
-CREATE PROCEDURE RoguelikeGames
-AS
-	BEGIN
-		SELECT GL.Game, GL.Genre, GL.Platform, GL.ReleaseYear
-		FROM GameLog GL
-		WHERE GL.Genre LIKE '%Roguelike%'
-	END;
-GO
-
--- List all other genres & all other games
+	
 -- Make backlog table and add all backlog games
 
 -- INSERTION STATEMENTS FOR DATA FROM EXCEL
@@ -203,5 +197,37 @@ EXEC InsertGameLogData 'Slay the Spire', 'Roguelike-Card Game-Indie', 'Playstati
 EXEC InsertGameLogData 'Journey', 'Adventure-Indie', 'Playstation 4', 2012;
 EXEC InsertGameLogData 'My Little Pony: A Maretime Bay Adventure', 'Adventure-Indie', 'Playstation 5', 2022;
 EXEC InsertGameLogData 'Demon Slayer: Kimetsu no Yaiba – The Hinokami Chronicles', 'Action-Fighting', 'Playstation 5', 2021;
-
+EXEC InsertGameLogData 'The Last Of Us Part I', 'Action-Adventure', 'Playstation 5', 2022;
+EXEC InsertGameLogData 'Fast & Furious: Spy Racers Rise of SH1FT3RS', 'Racing-Action', 'Playstation 5', 2021;
+EXEC InsertGameLogData 'Nour: Play with your Food', 'Puzzle-Indie', 'Playstation 5', 2023;
+EXEC InsertGameLogData 'TMNT: Shredder’s Revenge', 'Action-Indie-Fighting', 'Playstation 5', 2022;
+EXEC InsertGameLogData 'Tunic', 'Action-Adventure-Indie', 'Playstation 5', 2022;
+EXEC InsertGameLogData 'Kena: Bridge of Spirits', 'Action-Adventure-Indie', 'Playstation 5', 2021;
+EXEC InsertGameLogData 'For Honor', 'Action-Adventure', 'Playstation 5', 2017;
+EXEC InsertGameLogData 'Marvels Midnight Suns', 'RPG-Action', 'Playstation 5', 2024;
+EXEC InsertGameLogData 'Spiritfarer', 'Management-Indie-Adventure', 'Playstation 5', 2020;
+EXEC InsertGameLogData 'Harry Potter: Quidditch Champions', 'Sports-Adventure', 'Playstation 5', 2023;
+EXEC InsertGameLogData 'Cat Quest', 'RPG-Indie', 'Playstation 4', 2017;
+EXEC InsertGameLogData 'Deaths  Door', 'Action-Adventure-Indie', 'Playstation 4', 2021;
+EXEC InsertGameLogData 'Jedi Survivor', 'Action-Adventure', 'Playstation 5', 2023;
+EXEC InsertGameLogData 'Gotham Knights', 'Action-Adventure', 'Playstation 5', 2023;
+EXEC InsertGameLogData 'Raji: An Ancient Epic', 'Action-Adventure-Indie', 'Playstation 5', 2020;
+EXEC InsertGameLogData 'Road 96', 'Adventure-Indie', 'Playstation 5', 2021;
+EXEC InsertGameLogData 'Call Of The Sea', 'Adventure-Indie', 'Playstation 5', 2020;
+EXEC InsertGameLogData 'What Remains Of Edith Finch', 'Adventure-Indie', 'Playstation 4', 2017;
+EXEC InsertGameLogData 'Tomb Raider', 'Action-Adventure', 'Playstation 4', 2013;
+EXEC InsertGameLogData 'Cult of the Lamb', 'Action-Adventure-Indie', 'Playstation 5', 2022;
+EXEC InsertGameLogData 'Uncharted Golden Abyss', 'Action-Adventure', 'Playstation Vita', 2008;
+EXEC InsertGameLogData 'Gris', 'Platformer-Puzzle-Indie', 'Playstation 4', 2018;
+EXEC InsertGameLogData 'Infamous Second Son', 'Action-Adventure', 'Playstation 3', 2014;
+EXEC InsertGameLogData 'The Devil Inside Me', 'Horror-Adventure', 'Playstation 5', 2019;
+EXEC InsertGameLogData 'The Plucky Squire', 'Action-Adventure-Indie', 'Playstation 5', 2023;
+EXEC InsertGameLogData 'Mystic Pillars', 'RPG-Indie', 'Playstation 5', 2023;
+EXEC InsertGameLogData 'Buzz Lightyear Toy Story 2', 'Action-Adventure', 'Playstation 4', 2023;
+EXEC InsertGameLogData 'Undertale', 'RPG-Indie', 'Playstation Vita', 2015;
+EXEC InsertGameLogData 'Astro Bot', 'Platformer-Indie', 'Playstation 5', 2018;
+EXEC InsertGameLogData 'Tiny Kin', 'Platformer-Indie', 'Playstation 5', 2022;
+EXEC InsertGameLogData 'The Wolf Among Us', 'Adventure-Episodic', 'Playstation Vita', 2014;
+EXEC InsertGameLogData 'Wolfenstein: The New Order', 'FPS-Action', 'Playstation 5', 2014;
+-- 2024 Completions
 GO
